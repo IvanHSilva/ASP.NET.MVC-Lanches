1d0736234105fc9e61e285d1a714eb696d0933ae
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-08-31 14:55:20 -0300

    Privacy page removed

--- VendasLanches/Controllers/HomeController.cs
+++ VendasLanches/Controllers/HomeController.cs
@@ -6,15 +6,7 @@ namespace VendasLanches.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
         public IActionResult Index() {
-            return View();
-        }
-
-        public IActionResult Privacy() {
             return View();
         }
 
