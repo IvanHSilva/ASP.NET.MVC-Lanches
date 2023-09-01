3d3df60e0f2c4388a5c89c7875ffd250d54ec92e
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-01 16:34:13 -0300

    Model classes

---
+++ VendasLanches/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace VendasLanches.Models; 
+public class Category {
+    
+    // Attributes
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    // Constructors
+    public Category(){}
+
+    public Category(int id, string name, string description) {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+}
---
+++ VendasLanches/Models/Snack.cs
@@ -0,0 +1,31 @@
+﻿namespace VendasLanches.Models; 
+
+public class Snack {
+
+    // Attributes
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Image { get; set; } = string.Empty;
+    public string Miniature { get; set; } = string.Empty;
+    public bool Favorite { get; set; }
+    public bool HaveStock { get; set; }
+    public string Category { get; set; } = string.Empty;
+
+    // Constructors
+    public Snack(){}
+
+    public Snack(int id, string name, string description, string details, double price, string image, string miniature, bool favorite, bool haveStock) {
+        Id = id;
+        Name = name;
+        Description = description;
+        Details = details;
+        Price = price;
+        Image = image;
+        Miniature = miniature;
+        Favorite = favorite;
+        HaveStock = haveStock;
+    }
+}
