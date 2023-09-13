460cb0b499b64788ab2b4b1cc21c47bd0b1a3b4d
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-13 19:38:40 -0300

    CartResume ViewComponent

---
+++ VendasLanches/Components/CartResume.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendasLanches.Models;
+using VendasLanches.ViewModels;
+
+namespace VendasLanches.Components; 
+
+public class CartResume : ViewComponent {
+
+    private readonly Cart _cart;
+
+    public CartResume(Cart cart) {
+        _cart = cart;
+    }
+
+    public IViewComponentResult Invoke() {
+
+        List<CartItem> itens = _cart.GetCartItems();
+        //List<CartItem> itens = new List<CartItem> {
+        //    new CartItem(),
+        //    new CartItem(),
+        //};
+        _cart.CartItems = itens;
+
+        CartViewModel cartVM = new CartViewModel {
+            Cart = _cart,
+            CartTotal = _cart.GetCartTotal(),
+        };
+
+        return View(cartVM);
+    }
+
+}
