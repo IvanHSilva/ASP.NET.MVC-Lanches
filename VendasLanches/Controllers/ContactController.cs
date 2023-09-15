d2e8e70f2ba70bdcb5e583f0bc3e873cb6bcc3c6
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-15 16:54:15 -0300

    EMail TagHelper

---
+++ VendasLanches/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasLanches.Controllers;
+
+public class ContactController : Controller {
+    
+    public IActionResult Index() {
+        return View();
+    }
+}
---
+++ VendasLanches/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VendasLanches.TagHelpers;
+
+public class EmailTagHelper : TagHelper {
+    public string EMail { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        output.TagName = "a";
+        output.Attributes.SetAttribute("href", "mailto:" + EMail);
+        output.Content.SetContent(Content);
+    }
+}
