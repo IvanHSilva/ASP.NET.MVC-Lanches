c883afb9c3ea14c265796938b99b68e57d497791
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-16 09:48:35 -0300

    CategoryMenu ViewComponent

---
+++ VendasLanches/Components/CategoryMenu.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendasLanches.Models;
+using VendasLanches.Repositories.Interfaces;
+
+namespace VendasLanches.Components; 
+
+public class CategoryMenu : ViewComponent {
+    
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryMenu(ICategoryRepository categoryRepository) {
+        _categoryRepository = categoryRepository;
+    }
+
+    public IViewComponentResult Invoke() {
+
+        IEnumerable<Category> categories = _categoryRepository.Categories.OrderBy(c => c.Name);
+        return View(categories);
+    }
+}
