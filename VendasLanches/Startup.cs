fc29573a9c2542145ea141d5a7670ee2e3f9012d
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-16 09:26:39 -0300

    SnackCategory Route

--- VendasLanches/Startup.cs
+++ VendasLanches/Startup.cs
@@ -48,6 +48,11 @@ public class Startup {
 
         app.UseEndpoints(endpoints => {
             endpoints.MapControllerRoute(
+                name: "snackCategory",
+                pattern: "Snack/{action}/{category?}",
+                defaults: new { Controller = "Snack", Action = "List" }
+            );
+            endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
         });
