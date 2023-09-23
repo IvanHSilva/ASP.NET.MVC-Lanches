e7279be6e4a42e3df1e1380cf9321a70b7abb8ac
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-23 17:05:40 -0300

    View Checkout

--- VendasLanches/Controllers/OrderController.cs
+++ VendasLanches/Controllers/OrderController.cs
@@ -21,6 +21,46 @@ public class OrderController : Controller {
 
     [HttpPost]
     public IActionResult Checkout(Order order) {
-        return View();
+        
+        int totalItems = 0;
+        double totalAmount = 0;
+
+        // Get cart items
+        List<CartItem> items = _cart.GetCartItems();
+        _cart.CartItems = items;
+
+        // Check if there are items on cart
+        if (_cart.CartItems.Count == 0) {
+            ModelState.AddModelError("", "Seu carrinho está vazio!!!!");
+        }
+
+        // Calculate amount of the cart
+        foreach(CartItem item in items) {
+            totalItems += item.Quantity;
+            totalAmount += (item.Snack.Price * item.Quantity);
+        }
+
+        // Populate order with cart data
+        order.Items = totalItems;
+        order.TotalOrder = totalAmount;
+
+        // Validate data
+        if (ModelState.IsValid) {
+            // create Order
+            _orderRepository.CreateOrder(order);
+
+            // define messages
+            ViewBag.CheckoutMessage = "Obrigado por comprar conosco! :-)";
+            ViewBag.TotalAmount = _cart.GetCartTotal();
+
+            // clear the cart
+            _cart.ClearCart();
+
+            // return View
+            return View("~/Views/Order/Order.cshtml", order);
+        }
+
+        // return View
+        return View(order);
     }
 }
