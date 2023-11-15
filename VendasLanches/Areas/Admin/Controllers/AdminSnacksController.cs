d747013796d2fb117765a1191ba5a9ad3cd64860
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-11-14 22:11:03 -0300

    AdminSnacks Controller + Views

---
+++ VendasLanches/Areas/Admin/Controllers/AdminSnacksController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VendasLanches.Context;
+using VendasLanches.Models;
+
+namespace VendasLanches.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class AdminSnacksController : Controller {
+        private readonly AppDbContext _context;
+
+        public AdminSnacksController(AppDbContext context) {
+            _context = context;
+        }
+
+        // GET: Admin/AdminSnacks
+        public async Task<IActionResult> Index() {
+            return _context.Snacks != null ?
+                        View(await _context.Snacks.ToListAsync()) :
+                        Problem("Entity set 'AppDbContext.Snacks'  is null.");
+        }
+
+        // GET: Admin/AdminSnacks/Details/5
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null || _context.Snacks == null) {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (snack == null) {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Create
+        public IActionResult Create() {
+            return View();
+        }
+
+        // POST: Admin/AdminSnacks/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Details,Price,Image,Miniature,Favorite,HaveStock,Category,RegDate")] Snack snack) {
+            
+            int id = 1;
+            Snack snc = _context.Snacks.OrderBy(s => s.Id).LastOrDefault()!;
+            if (snc != null) { id = snc.Id + 1; }
+
+            snack!.Id = id;
+
+            if (ModelState.IsValid) {
+                _context.Add(snack);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Edit/5
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null || _context.Snacks == null) {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks.FindAsync(id);
+            if (snack == null) {
+                return NotFound();
+            }
+            return View(snack);
+        }
+
+        // POST: Admin/AdminSnacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Details,Price,Image,Miniature,Favorite,HaveStock,Category,RegDate")] Snack snack) {
+            if (id != snack.Id) {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid) {
+                try {
+                    _context.Update(snack);
+                    await _context.SaveChangesAsync();
+                } catch (DbUpdateConcurrencyException) {
+                    if (!SnackExists(snack.Id)) {
+                        return NotFound();
+                    } else {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Delete/5
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null || _context.Snacks == null) {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (snack == null) {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // POST: Admin/AdminSnacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            if (_context.Snacks == null) {
+                return Problem("Entity set 'AppDbContext.Snacks'  is null.");
+            }
+            var snack = await _context.Snacks.FindAsync(id);
+            if (snack != null) {
+                _context.Snacks.Remove(snack);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SnackExists(int id) {
+            return (_context.Snacks?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- VendasLanches/Models/Snack.cs
+++ VendasLanches/Models/Snack.cs
@@ -9,19 +9,24 @@ public class Snack {
     // Attributes
     [Key]
     public int Id { get; set; }
-    [Column("Nome")][Required, MaxLength(50)] 
+    [Column("Nome")][MaxLength(50)]
+    [Required(ErrorMessage = "Nome obrigatório")]
     public string Name { get; set; } = string.Empty;
     [Column("Descricao")]
     [Display(Name="Descrição")]
-    [Required, MaxLength(100)]
+    [MaxLength(100)]
+    [Required(ErrorMessage = "Descrição obrigatória")]
     public string Description { get; set; } = string.Empty;
-    [Column("Detalhes")][Required, MaxLength(250)]
+    [Column("Detalhes")][MaxLength(250)]
+    [Required(ErrorMessage = "Detalhes obrigatórios")]
     public string Details { get; set; } = string.Empty;
-    [Column("Preco")][Required]
+    [Column("Preco")]
     [Display(Name = "Preço")]
+    [DataType(DataType.Currency, ErrorMessage = "Preço inválido!")]
+    [Required(ErrorMessage = "Preço obrigatório", AllowEmptyStrings = false)]
     [Range(1, 199.99, ErrorMessage = "O preço deve estar ente R$ 1,00 e R$ 199,99")]
     public double Price { get; set; }
-    [Column("Imagem")][MaxLength(150)] 
+    [Column("Imagem")][MaxLength(150)]
     public string Image { get; set; } = string.Empty;
     [Column("Miniatura")][MaxLength(150)] 
     public string Miniature { get; set; } = string.Empty;
@@ -31,11 +36,14 @@ public class Snack {
     [Display(Name = "Tem Estoque")]
     [Required]
     public bool HaveStock { get; set; }
-    [Column("Categoria")][Required, MaxLength(50)] 
+    [Column("Categoria")][MaxLength(50)]
+    [Required(ErrorMessage = "Categoria obrigatória")]
     public string Category { get; set; } = string.Empty;
     [Column("DataCad")]
     [Display(Name = "Data de Cadastro")]
-    [Required]
+    [DataType(DataType.Date, ErrorMessage = "Data inválida!")]
+    [Required(ErrorMessage = "Data de Cadastro Inválida!")]
+    [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
     public DateTime RegDate { get; set; }
 
     // Constructors
